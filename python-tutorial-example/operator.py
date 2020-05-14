806f9ee13a99c754f0cdde57372cf4688cd5f139
himanshu <himanshudogra96@gmail.com>
2020-05-14 19:05:15 +0530

    operator program

---
+++ python-tutorial-example/operator.py
@@ -0,0 +1,11 @@
+word=input('Enter something: ')
+
+if word == 'hi':
+    word = 'hi '
+    word *= 1000
+    print(word )
+
+    
+if word == 'i love you':
+    word *= 1000000
+    print(word)
---
+++ python-tutorial-example/operator2.py
@@ -0,0 +1,7 @@
+
+w1=input('enter first word: ')
+w2=input('enter second word: ')
+
+
+word = w1 +" "+ w2+" "
+print(word * 1000)
