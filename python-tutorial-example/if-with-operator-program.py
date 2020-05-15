56c1dc7037ee10cf57a2039f52ac1c0a0bee117b
himanshu <himanshudogra96@gmail.com>
2020-05-15 10:54:00 +0530

    if + operator

---
+++ python-tutorial-example/if-with-operator-program.py
@@ -0,0 +1,11 @@
+#Other things in String, 
+
+word=input("Please enter something: ")
+
+if "hi" in word:
+    print("hello!")
+elif 'hello' not in word:
+    print("GoLo "*10000)
+else:
+    CHOCHO="JOJO "+"MOJO "
+    print(CHOCHO * 1000000)
