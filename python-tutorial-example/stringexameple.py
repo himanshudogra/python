729eec8f2601aad708bfff5bc1c5912d7d02d3c7
himanshu <himanshudogra96@gmail.com>
2020-05-17 12:55:30 +0530

    Updated

--- python-tutorial-example/stringexameple.py
+++ python-tutorial-example/stringexameple.py
@@ -3,6 +3,7 @@ word1 = input("Enter something: ")
 word2 = input("Enter another thing: ")
 word3 = input("Enter a third thing: ")
 word4 = input("And yet another thing: ")
+
 print("You entered " + word1 + ", " + word2 + ", " + word3 + " and " + word4 + ".")
 
 print('You entered {}, {}, {} and {}.'.format(word1,word2,word3,word4))
