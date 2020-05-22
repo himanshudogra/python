a37f4c29407575bbf41f3ac152d6defbe904686d
himanshu <himanshudogra96@gmail.com>
2020-05-22 15:38:31 +0530

    enumeration example

---
+++ python-tutorial-example/enumeration.py
@@ -0,0 +1,3 @@
+presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson"]
+for num, name in enumerate(presidents, start=1):
+    print("President %s: %s"% (num,name))
